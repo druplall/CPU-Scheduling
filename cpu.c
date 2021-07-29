a240b42fb8dd970c22640e5d6ab4d254058e1060
Deo-Mac <druplall@gmail.com>
2021-07-29 11:31:02 -0400

    Wip

--- cpu.c
+++ cpu.c
@@ -24,5 +24,14 @@ int main(){
   new_process.process_priority = 6;
   struct PCB temp = handle_process_arrival_pp(ready_queue, &queue_cnt, current_process, new_process, 2);
 
+
+   printf("ready_queue: ");
+   for(int i=0;i<queue_cnt;i++)
+       printf("[PID:%d, AT:%d, TBT:%d, EST:%d, EET:%d, RBT:%d, Priority:%d]\n",ready_queue[i].process_id,ready_queue[i].arrival_timestamp,ready_queue[i].total_bursttime,ready_queue[i].execution_starttime,ready_queue[i].execution_endtime,ready_queue[i].remaining_bursttime,ready_queue[i].process_priority);
+
+       printf("queue_cnt: %d\n",queue_cnt);
+       printf("PCB: ");
+       printf("[PID:%d, AT:%d, TBT:%d, EST:%d, EET:%d, RBT:%d, Priority:%d]\n",temp.process_id,temp.arrival_timestamp,temp.total_bursttime,temp.execution_starttime,temp.execution_endtime,temp.remaining_bursttime,temp.process_priority);
+
   return 0;
 }
