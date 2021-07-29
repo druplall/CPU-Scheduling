08e3e7f47a19a312fe2ddaaa8697b8699871899e
Deo-Mac <druplall@gmail.com>
2021-07-29 11:32:36 -0400

    WI

--- oslabs.h
+++ oslabs.h
@@ -67,36 +67,3 @@ struct PCB handle_process_completion_srtp(struct PCB ready_queue[QUEUEMAX], int 
 struct PCB handle_process_arrival_rr(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, struct PCB current_process, struct PCB new_process, int timestamp, int time_quantum);
 struct PCB handle_process_completion_rr(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, int timestamp, int time_quantum);
 
-struct PCB handle_process_arrival_pp(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, struct PCB current_process, struct PCB new_process, int timestamp)
-{
-    //NULLPCB
-    if ((current_process.process_id == 0) && (current_process.arrival_timestamp == 0) && (current_process.total_bursttime == 0) && (current_process.execution_starttime == 0) && (current_process.execution_endtime == 0) && (current_process.remaining_bursttime == 0) && (current_process.process_priority == 0))
-    {
-        new_process.execution_starttime = timestamp;
-        new_process.execution_endtime = timestamp + new_process.total_bursttime;
-        new_process.remaining_bursttime = new_process.total_bursttime;
-        return new_process;
-    }
-    else if (new_process.process_priority >= current_process.process_priority)
-    {
-        new_process.execution_starttime = 0;
-        new_process.execution_endtime = 0;
-        new_process.remaining_bursttime = new_process.total_bursttime;
-        ready_queue[*queue_cnt] = new_process;
-        (*queue_cnt)++;
-        return current_process;
-    }
-    else
-    {
-        new_process.execution_starttime = timestamp;
-        new_process.execution_endtime = timestamp + new_process.total_bursttime;
-        new_process.remaining_bursttime = new_process.total_bursttime;
-
-        current_process.execution_endtime = 0;
-        current_process.remaining_bursttime = current_process.remaining_bursttime - 1;
-        ready_queue[*queue_cnt] = current_process;
-        (*queue_cnt)++;
-
-        return new_process;
-    }
-}
